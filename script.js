4f4deef5c00b7881849160d7ed9876502ba6106e
Jayashree-M-ai <jayashree0508@gmail.com>
2026-06-02 20:35:34 +0530

    script.js

--- script.js
+++ script.js
@@ -20,8 +20,6 @@ function uploadFile() {
             const formData = new FormData();
 
             formData.append("file", file);
-
-                   
             formData.append(
                 "lat",
                 position.coords.latitude
@@ -40,8 +38,6 @@ function uploadFile() {
             const result =
                 await response.json();
 
-            console.log("OUTPUT FILE:", result.output_file);         
-
             console.log(result);
             alert(JSON.stringify(result));
 
@@ -51,21 +47,14 @@ function uploadFile() {
             const resultDiv =
                 document.getElementById("result");
 
-           
-            const fileUrl = "https://pothole-detection-system-1-7uj2.onrender.com" + result.output_file;
             if (
                 file.type.startsWith("image/")
             ) {
 
                 resultDiv.innerHTML = `
                 <h3>Output Image</h3>
-                <p><b>Potholes Detected:</b> ${result.count}</p>
-                <p><b>GPS:</b>
-                ${position.coords.latitude},
-                ${position.coords.longitude}
-                </p>
                 <img
-                src="${fileUrl}"
+                src="https://pothole-detection-system-1-7uj2.onrender.com/${result.output_file}"
                 width="100%">
                 `;
 
@@ -73,15 +62,9 @@ function uploadFile() {
 
                 resultDiv.innerHTML = `
                 <h3>Output Video</h3>
-                <p><b>Potholes Detected:</b> ${result.count}</p>
-                <p><b>GPS:</b>
-                ${position.coords.latitude},
-                ${position.coords.longitude}
-                </p>
                 <video width="100%" controls>
                 <source
-                src="${fileUrl}"
-                width="100%">
+                src="https://pothole-detection-system-1-7uj2.onrender.com/${result.output_file}">
                 </video>
                 `;
             }
